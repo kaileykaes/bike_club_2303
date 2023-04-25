b5d0f0dc93b016cd3a16977ff38476ee8c9d3bb9
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-25 09:12:40 -0600

    Feat: #initialize

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,11 @@
 class Ride
-
+  attr_reader :name,
+              :distance, 
+              :terrain
+  
+  def initialize(attributes)
+    @name = attributes[:name]
+    @distance = attributes[:distance]
+    @terrain = attributes[:terrain]
+  end
 end
