7d3a39fdcf87b4fdccda9517092f622b66ac9d16
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-25 09:17:41 -0600

    Feat: #loop?

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,11 +1,17 @@
 class Ride
   attr_reader :name,
               :distance, 
-              :terrain
+              :terrain, 
+              :loop
   
   def initialize(attributes)
     @name = attributes[:name]
     @distance = attributes[:distance]
     @terrain = attributes[:terrain]
+    @loop = attributes[:loop]
+  end
+
+  def loop?
+    @loop
   end
 end
